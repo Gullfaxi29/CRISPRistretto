0619f46e5b449ba00f51d9eacce5ab91c7059354
abarnett-ds <76080451+abarnett-ds@users.noreply.github.com>
2024-02-01 14:17:34 +1100

    Initial Commit -> started CRISPRistettoPooled module with some preliminary changes.

---
+++ CRISPRistrettoPooled.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+
+Adapted from
+CRISPResso2 - Kendell Clement and Luca Pinello 2018
+Software pipeline for the analysis of genome editing outcomes from deep sequencing data
+(c) 2018 The General Hospital Corporation. All Rights Reserved.
+
+Alex Barnett 2024
+'''
+
+
+from CRISPResso2.CRISPRistrettoPooledCORE import main
+
+
+if __name__ == '__main__':
+    main()
